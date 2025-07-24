8b462794f8ea749ddffca37764dc1d5a843c3892
Alonso <alonso.antunez@improma.com>
2025-07-23 18:28:20 -0600

    responsive updates for 'coming soon'

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -7,13 +7,13 @@ import logoLive from '../assets/img/tikTokCopaLive-LogoHeader.png'
 function ComingSoon() {
   return (
     <>
-       <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 imagecontainer logoLive d-flex flex-row justify-content-end'>
+       <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 pe-md-0 pe-lg-5 pe-xl-5 pe-xxl-5 imagecontainer logoLive d-flex flex-row justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-end'>
         <img src={logoLive} alt="TikTok Live" />
       </div>
-      <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 imagecontainer logoSoonContainer'>
+      <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 imagecontainer logoSoonContainer mb-5'>
         <img src={tiktokCopaLiveLogo} alt="Logo TikTok Copa Live" />
       </div>
-      <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 imagecontainer soonContainer'>
+      <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6 imagecontainer soonContainer ps-5 pe-5'>
         <img src={Soon} alt="Coming Soon" />
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'         ❌ desactiva App temporalmente
+import App from './App'        
 import ComingSoon from './components/ComingSoon.jsx' // ✅ usa esta página
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+import './breakpoints.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
